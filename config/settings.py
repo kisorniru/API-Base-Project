1bc4cfaba3c740fbe833b9f576bb70ae130a8574
kisorniru <kisorniru@gmail.com>
2024-01-10 19:51:00 +0600

    Added fresh Django project structure

---
+++ config/settings.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'change-me-in-env'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'rest_framework',
+]
+
+MIDDLEWARE = []
+ROOT_URLCONF = 'config.urls'
+TEMPLATES = []
+WSGI_APPLICATION = 'config.wsgi.application'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
