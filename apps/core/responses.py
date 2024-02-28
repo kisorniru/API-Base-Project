865c4b3f14b6bd10e7e79db9eef6e9b9788c4f87
kisorniru <kisorniru@gmail.com>
2024-02-28 19:49:00 +0600

    Cleaned response helper names

--- apps/core/responses.py
+++ apps/core/responses.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 
-def api_response(data=None, message='OK', status_code=200):
+def build_response(data=None, message='OK', status_code=200):
     return Response({
         'success': status_code < 400,
         'message': message,
