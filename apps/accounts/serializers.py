661d670bc1b419a7e5a727632fc6b8c26c8b1c90
kisorniru <kisorniru@gmail.com>
2024-04-16 21:33:00 +0600

    Added register serializer example

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True, trim_whitespace=False)
+
+class RegisterSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, min_length=8, trim_whitespace=False)
