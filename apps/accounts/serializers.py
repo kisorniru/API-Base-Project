2d8752d3e4c01d1e53d471e1be4e38993183bce2
kisorniru <kisorniru@gmail.com>
2024-05-22 22:37:00 +0600

    Added user serializer example

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -8,3 +8,8 @@ class RegisterSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=150)
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True, min_length=8, trim_whitespace=False)
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    email = serializers.EmailField(required=False)
