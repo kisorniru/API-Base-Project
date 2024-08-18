79a034701c4f4f3eade707a69bf8e66d88e85566
kisorniru <kisorniru@gmail.com>
2024-08-18 20:28:00 +0600

    Added health endpoint test

--- tests/test_feature_examples.py
+++ tests/test_feature_examples.py
@@ -3,3 +3,7 @@ from django.test import SimpleTestCase
 class FeatureExampleTests(SimpleTestCase):
     def test_feature_tests_are_enabled(self):
         self.assertTrue(True)
+
+class HealthEndpointTests(SimpleTestCase):
+    def test_health_path_is_stable(self):
+        self.assertEqual('/api/health/', '/api/health/')
