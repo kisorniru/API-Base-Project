18f19be00d207e8035157de5f465af9c16fc7e5d
kisorniru <kisorniru@gmail.com>
2024-08-27 19:59:00 +0600

    Added validation response test

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -7,3 +7,11 @@ class ResponseHelperTests(SimpleTestCase):
         response = build_response({'ok': True})
         self.assertTrue(response.data['success'])
         self.assertEqual(response.data['data'], {'ok': True})
+
+from apps.core.responses import validation_error_response
+
+class ValidationResponseTests(SimpleTestCase):
+    def test_validation_response_uses_422(self):
+        response = validation_error_response({'email': ['Required']})
+        self.assertEqual(response.status_code, 422)
+        self.assertFalse(response.data['success'])
